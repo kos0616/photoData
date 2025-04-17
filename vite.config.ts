a159ca96a586501ce7372d3980e5910ad0c5f100
Mark Liang <mark@vdr.tw>
2025-04-17 16:01:31 +0800

    feat: publich

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/photoData",
+  build: {
+    outDir: "docs",
+  },
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
